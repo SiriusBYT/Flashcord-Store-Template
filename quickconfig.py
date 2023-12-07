25ba1b6924a82db5302b24194ac0363e1fcdf73f
Sirius B <root.siriusb@gmail.com>
2023-12-07 12:23:53 +0100

    Create quickconfig.py

---
+++ quickconfig.py
@@ -0,0 +1,14 @@
+"""
+This tool lets you quickly configure your page in a hurry.
+Currently does not work/untested. Please do not use in production.
+either way this does literally nothing rn lol cuz me lazy lul
+"""
+
+Name = "Extension Name"
+Version = "v1.0"
+License = "Unlicense"
+Discord = "https://discord.gg/"
+GitHub_User = "https://github.com/Module-Developer"
+
+IsPlugin = False
+
