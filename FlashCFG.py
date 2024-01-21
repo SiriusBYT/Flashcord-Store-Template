6971aac7d4f99ee93b2f288dc3569a8b7cfabff9
Sirius B <root.siriusb@gmail.com>
2024-01-21 01:19:00 +0100

    wait what it works?
    will push to stable tomorrow

--- FlashCFG.py
+++ FlashCFG.py
@@ -8,9 +8,9 @@ This script does not do the following but will in the future:
 
 # Edit the following things
 IsRepluggedPlugin = False
-IsRepluggedTheme = False # TBD, does absolutely nothing for now
+IsFlashcordCompetitor = False # TBD, does absolutely nothing for now (Pages for Themes, yes the Flashcord store will... host themes other than Flashcord. This is a stupid idea which is why this isn't implemented yet and why I'm still just thinking about it.)
 
-Name = "FlashCFG-built Store Template"
+Name = "FlashCFG-Built Store Template"
 Short_Description = "This store page was created using the Flashcord Store Quick Config Python Script!"
 Version = "v1.1.0"
 License_Year = "2024"
@@ -30,18 +30,19 @@ Folder_Name = "module_template-files" # NO capitals! Underscores only! Must have
 Embed_FileName = "embed-banner.png" # Notice: GIFs work!
 Store_Embed_FileName = "embed-banner.png" # I would still suggest against it due to AuraCloud-E2A's limited space.
 
-Long_Description = '<p>Long descriptions are actually quite the doozy, they require actual HTML code to be set inside this little variable. \
-While quite impractical looking, it lets you entirely skip the manual process of opening your Store Page ENTIRELY!</p> \
-<p class="SNDL-ParenthesizedText">This has limitations though... Of course...</p> \
-<p>You obviously need HTML and SNDL knowledge if you wanna make this look as nice as possible.</p> \
-<p>But of course since this is just a python variable inserted into HTML code that means that yes, you can:</p> \
-<div class="SNDL-DashList"> \
-    <p>Create certain parts of your long description to have parts of your module/plugin info update itself when you rebuild your Store page! For example this is version ' + Version + ' of FlashCFG that built this page!</p> \
-    <p>And you can probably do a lot more but since I ran out of ideas, well you go figure it out yourself.</p> \
+Long_Description = '<p>Long descriptions are actually quite the doozy, they require actual HTML code to be set inside this little variable. \n \
+While quite impractical looking, it lets you entirely skip the manual process of opening your Store Page HTML ENTIRELY!</p> \n \
+<p class="SNDL-ParenthesizedText">This has limitations though... Of course...</p> \n \
+<p>You obviously need HTML and SNDL knowledge if you wanna make this look as nice as possible.</p> \n \
+<p>But of course since this is just a python variable inserted into HTML code that means that yes, you can:</p> \n \
+<div class="SNDL-DashList"> \n \
+    <p>Create certain parts of your long description to have parts of your module/plugin info update itself when you rebuild your Store page! For example this is version ' + Version + ' of FlashCFG that built this page!</p> \n \
+    <p>And you can probably do a lot more but since I ran out of ideas, well you go figure it out yourself.</p> \n \
 </div>'
 
 # NOT recommended to modify, do this only if you know what you're doing! 
-StoreTemplate = "flashcord/store/templates/default/default_template.html"
+StoreTemplate = "flashcord/store/templates/default/default-store_template.html"
+EmbedTemplate = "flashcord/store/templates/default/default-embed_template.html"
 
 # Don't touch this, it will get overwritten anyways but still. Don't touch just in case.
 HTMLFile = ""
@@ -71,10 +72,15 @@ def FileBackup(FileToBackup):
     print("[FlashCFG // Backup] The", FileToBackup, "will now be backed up...")
     HTMLFile = GetHTMLFile(FileToBackup)
     HTMLFile_Backup = HTMLFile.replace(".html",".bak-html")
-    with open(HTMLFile, 'r', encoding='utf-8') as HTMLFile_File:
-        with open(HTMLFile_Backup, 'w', encoding='utf-8') as HTMLFile_Backup_File:
-            HTMLFile_Backup_File.write((HTMLFile_File.read()))
-    print("[FlashCFG // Backup] ", HTMLFile_Backup, "has been created and is now backed up.")
+    try:
+        with open(HTMLFile, 'r', encoding='utf-8') as HTMLFile_File:
+            with open(HTMLFile_Backup, 'w', encoding='utf-8') as HTMLFile_Backup_File:
+                HTMLFile_Backup_File.write((HTMLFile_File.read()))
+        print("[FlashCFG // Backup] ", HTMLFile_Backup, "has been created and is now backed up.")
+    except:
+        print("[FlashCFG // Backup] ", HTMLFile_Backup, "doesn't exist! Creating empty file instead...")
+        with open(HTMLFile, 'w', encoding='utf-8') as EditHTML_File:
+            EditHTML_File.write("")
 
 def HTMLConfigurator(Step):
     # We're doing this the MarkSNDL way, I can't fucking figure out how to do this the objectively better way
@@ -89,37 +95,45 @@ def HTMLConfigurator(Step):
         HTMLFile = GetHTMLFile("Embed")
         StepFile = "// Embed"
     else:
-        print("[FlashCFG // Backup] WARNING: Sirius A was here and replaced Sirius B's Yae wallpaper with a Kirara one.", '(What the fuck is Step "', Step, '"?!)')
+        print("[FlashCFG // HTML-CFG] ERROR: Sirius A was here and replaced Sirius B's Yae wallpaper with a Kirara one.", '(What the fuck is Step "', Step, '"?!)')
         return "FUCK"
 
     with open(StoreTemplate, 'r', encoding='utf-8') as StoreTemplate_File:
-        with open(HTMLFile, 'w', encoding='utf-8') as EditHTML_File:
-            EditHTML_File.write("")
-        with open(HTMLFile, 'a', encoding='utf-8') as EditHTML_File:
-            HTMLArray = StoreTemplate_File.readlines()
-            for line in range (len(HTMLArray)):
-                # print('[FlashCGG] Processing Line"', line, '" which is "', HTMLArray[line], '".')
-                HTMLArray[line] = HTMLArray[line].replace("[NAME]", Name)
-                HTMLArray[line] = HTMLArray[line].replace("[SHORT_DESC]", Short_Description)
-                HTMLArray[line] = HTMLArray[line].replace("[LONG_DESC]", Short_Description)
-                HTMLArray[line] = HTMLArray[line].replace("[VERSION]", Version)
-                HTMLArray[line] = HTMLArray[line].replace("[LICENSE_YEAR]", License_Year)
-                HTMLArray[line] = HTMLArray[line].replace("[LICENSE]", License)
-                HTMLArray[line] = HTMLArray[line].replace("[GITHUB_PROFILE]", GitHub_Profile)
-                HTMLArray[line] = HTMLArray[line].replace("[GITHUB_REPO]", GitHub_Repo)
-                HTMLArray[line] = HTMLArray[line].replace("[GITHUB_CONTRIBUTORS]", GitHub_Contributors)
-                HTMLArray[line] = HTMLArray[line].replace("[DISCORD_LINK]", Discord)
-                HTMLArray[line] = HTMLArray[line].replace("[THEME]", SNDL_Theme)
-                HTMLArray[line] = HTMLArray[line].replace("[EMBED_COLOR]", Embed_Color)
-                HTMLArray[line] = HTMLArray[line].replace("[STORE_PAGE_NAME]", Store_Page_Name)
-                HTMLArray[line] = HTMLArray[line].replace("[FOLDER_NAME]", Folder_Name)
-                HTMLArray[line] = HTMLArray[line].replace("[EMBED_FILENAME]", Embed_FileName)
-                HTMLArray[line] = HTMLArray[line].replace("[STORE_EMBED_FILENAME]", Store_Embed_FileName)
-                HTMLArray[line] = HTMLArray[line].replace("[STORE_USER_FOLDER]", UserFolderName)
-                EditHTML_File.write(HTMLArray[line])
-                ProcessingProgress = (line/len(HTMLArray))*100
-                print('[FlashCFG // HTML-CFG] Proccessed Line', line, '/', len(HTMLArray), '(', ProcessingProgress, '%).', StepFile)
-                # print('[FlashCGG] Processed Line is now "', HTMLArray[line], '".')
+        with open(EmbedTemplate, 'r', encoding='utf-8') as EmbedTemplate_File:
+            with open(HTMLFile, 'w', encoding='utf-8') as EditHTML_File:
+                EditHTML_File.write("")
+            with open(HTMLFile, 'a', encoding='utf-8') as EditHTML_File:
+                if Step == 0:
+                    HTMLArray = StoreTemplate_File.readlines()
+                elif Step == 1:
+                    HTMLArray = EmbedTemplate_File.readlines()
+                else:
+                    print("[FlashCFG // HTML-CFG] WARNING: Sirius A was here and caused another Big Bang", '(What the fuck is Step "', Step, '"?!)')
+                    print("[FlashCFG // HTML-CFG] Also how in the LIVING FUCK DID YOU TRIGGER THIS ERROR without triggering the previous one?")
+                    return "FUCK"
+                for line in range (len(HTMLArray)):
+                    # print('[FlashCGG] Processing Line"', line, '" which is "', HTMLArray[line], '".')
+                    HTMLArray[line] = HTMLArray[line].replace("[NAME]", Name)
+                    HTMLArray[line] = HTMLArray[line].replace("[SHORT_DESC]", Short_Description)
+                    HTMLArray[line] = HTMLArray[line].replace("[LONG_DESC]", Long_Description)
+                    HTMLArray[line] = HTMLArray[line].replace("[VERSION]", Version)
+                    HTMLArray[line] = HTMLArray[line].replace("[LICENSE_YEAR]", License_Year)
+                    HTMLArray[line] = HTMLArray[line].replace("[LICENSE]", License)
+                    HTMLArray[line] = HTMLArray[line].replace("[GITHUB_PROFILE]", GitHub_Profile)
+                    HTMLArray[line] = HTMLArray[line].replace("[GITHUB_REPO]", GitHub_Repo)
+                    HTMLArray[line] = HTMLArray[line].replace("[GITHUB_CONTRIBUTORS]", GitHub_Contributors)
+                    HTMLArray[line] = HTMLArray[line].replace("[DISCORD_LINK]", Discord)
+                    HTMLArray[line] = HTMLArray[line].replace("[THEME]", SNDL_Theme)
+                    HTMLArray[line] = HTMLArray[line].replace("[EMBED_COLOR]", Embed_Color)
+                    HTMLArray[line] = HTMLArray[line].replace("[STORE_PAGE_NAME]", Store_Page_Name)
+                    HTMLArray[line] = HTMLArray[line].replace("[FOLDER_NAME]", Folder_Name)
+                    HTMLArray[line] = HTMLArray[line].replace("[EMBED_FILENAME]", Embed_FileName)
+                    HTMLArray[line] = HTMLArray[line].replace("[STORE_EMBED_FILENAME]", Store_Embed_FileName)
+                    HTMLArray[line] = HTMLArray[line].replace("[STORE_USER_FOLDER]", UserFolderName)
+                    EditHTML_File.write(HTMLArray[line])
+                    ProcessingProgress = ((line+1)/len(HTMLArray))*100
+                    print('[FlashCFG // HTML-CFG] Processed Line', line+1, '/', len(HTMLArray), '(', ProcessingProgress, '%).', StepFile)
+                    # print('[FlashCGG] Processed Line is now "', HTMLArray[line], '".')
 
 def FlashcordStoreConfig():
     print("[FlashCFG] Script initiated.")
